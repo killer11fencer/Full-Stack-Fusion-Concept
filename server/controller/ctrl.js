2e183c2dd48c70cbe7a4269d8ea7036205cc5930
Jeoffrey <jeoff.howe@gmail.com>
2019-05-06 11:08:20 -0600

    created getone

--- server/controller/ctrl.js
+++ server/controller/ctrl.js
@@ -5,5 +5,10 @@ module.exports = {
         db.getAllDishes().then(result=>res.status(200).send(result)).catch(err=>console.log('err on get',err))
 
     },
-        
+    getOneDish: (req,res) => {
+        const db = req.app.get('db')
+        const {id} = req.params
+        db.getOneDish({id}).then(result=>res.status(200).send(result)).catch(err=>console.log('err on getOne',err))
+    }
+
 }
