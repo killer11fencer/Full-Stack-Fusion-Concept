a0bfa4d712cda69ba6d283089d44d82dce626a7d
Jeoffrey <jeoff.howe@gmail.com>
2019-05-06 12:11:09 -0600

    created controllers

--- db/registerUser.sql
+++ db/registerUser.sql
@@ -2,7 +2,7 @@ insert into user_login (username,password)
 values(
     ${username},
     ${hash}
-) returning username, password;
+) returning username,
 
 insert into users (first_name,last_name,email,phone)
 values (
--- server/controller/ctrl.js
+++ server/controller/ctrl.js
@@ -9,6 +9,50 @@ module.exports = {
         const db = req.app.get('db')
         const {id} = req.params
         db.getOneDish({id}).then(result=>res.status(200).send(result)).catch(err=>console.log('err on getOne',err))
-    }
+    },
+    register: async (req,res) => {
+        const db = req.app.get('db')
+        const {email,first_name,last_name,username,password,phone} = req.body
+        const {session} = req
+        let emailTaken = await db.checkEmail({email})
+        emailTaken = +emailTaken[0].count
+      console.log()
+        if(emailTaken !== 0) { return res.sendStatus(409)}
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password,salt)
+        const user_id = await db.registerUser({
+            email,
+            first_name,
+            last_name,
+            username,
+            hash,
+            phone
+        })
+        session.user = {
+            username,
+            first_name
+            
+        }
+        res.sendStatus(200)
+    },
+login: async (req,res) => {
+    const db = req.app.get('db')
+    const {loginUsername:username} = req.body
+    const {session} = req
+    try {
+    let user = await db.login({username})
+    session.user = user[0]
+    const authenticated = bcrypt.compareSync(req.body.loginPassword,user[0].password)
+    if(authenticated){
+        res.status(200).send({authenticated,user_id:user[0].login_id})
+    } else { throw new Error(401,'Error')} 
+}catch(err) {res.sendStatus(401)}
+},
+logOut: (req,res) => {
+    req.session.destroy()
+    res.sendStatus(200)
+}
+
+
 
 }
