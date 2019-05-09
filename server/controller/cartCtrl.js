73e3fda79c3d7da96cf08a8434e22f4867f8b1a6
Jeoffrey <jeoff.howe@gmail.com>
2019-05-09 11:34:46 -0600

    cart

--- server/controller/cartCtrl.js
+++ server/controller/cartCtrl.js
@@ -1,4 +1,9 @@
 module.exports = {
+
+    getCart: async (req,res) => {
+        let {cart} = req.session
+        res.status(200).send(cart)
+    },
     add: async (req,res) => {
         let {cart} = req.session
         let {dish_id,dish_name,img,description,price,quantity} = req.body        
@@ -12,5 +17,31 @@ module.exports = {
             cart.total += price }
         req.session.save()
         console.log('caaart',cart)
+        res.status(200)
+    },
+    remove: async (req,res) => {
+        let{cart} = req.session
+        let {dish_id,price,quantity} = req.body
+        const index = await cart.cart.findIndex(dish=>dish.dish_id === dish_id)
+        cart.cart.splice(index,1)
+        cart.total -= (price * quantity)
+        req.session.save()
+        res.status(200)
+    },
+   update: async (req,res) => {
+        let {cart} = req.session
+        let {dish_id,price,quantity} = req.body
+        const index = await cart.cart.findIndex(dish=>dish.dish_id === dish_id)
+        if(cart.cart[index].quantity < quantity) {
+            cart.cart[index].quantity += quantity
+            cart.total += (quantity * price)
+        } else {cart.cart[index].quantity -= quantity
+            cart.total -= (quantity * price)
+        }
+        req.session.save()
+        return res.status(200)
     }
+
+            
 }
+
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class Cart extends Component {
     constructor() {
         super();
         this.state = {
-            cart:[]
+            cart:[],
+            total: null,
+            userInput: null
         }
     }
+    componentDidMount() {
+        axios.get('/api/cart').then(res=> this.setState({cart: res.data.cart, total: res.data.total}))
+
+    }
     render() {
+        console.log(this.state.cart)
+        let displayCart = this.state.cart.map((item,id)=>{
+            return <div key={id}><h5>{item.dish_name}</h5>
+            <img width='50px'src={item.img}/>
+            <div>Quantity: <input name='userInput' value={item.quantity} 
+            onChange={this.handleChange}/></div>
+            <button onClick={this.updateCart}>Update</button>
+            <button onClick={this.deleteCart}>Delete</button>
+            </div>
+        })
+        
         return(
+<div>{displayCart}
 <div></div>
+</div>
         )
     }
 }
