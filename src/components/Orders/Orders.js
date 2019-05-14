02b2b199acee2ecefd845884df3b0993f7160191
Jeoffrey <jeoff.howe@gmail.com>
2019-05-14 12:06:01 -0600

    styling

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Popup from 'reactjs-popup'
 
 
@@ -11,55 +11,59 @@ class Orders extends Component {
         this.state = {
             orders: [],
             status: '',
-            notes:''
+            notes: ''
         }
     }
     componentDidMount() {
-        if(!this.props.admin && this.props.authenticated) { return this.getAllOrders()}
-        if(this.props.admin) {return this.getAllOrdersAdmin()}
+        if (!this.props.admin && this.props.authenticated) { return this.getAllOrders() }
+        if (this.props.admin) { return this.getAllOrdersAdmin() }
     }
     getAllOrders = async () => {
-        axios.get('/api/orders').then(res=>this.setState({orders: res.data}))
+        axios.get('/api/orders').then(res => this.setState({ orders: res.data }))
     }
     getAllOrdersAdmin = () => {
-        axios.get('/api/admin/orders').then(res=>this.setState({orders: res.data}))
+        axios.get('/api/admin/orders').then(res => this.setState({ orders: res.data }))
     }
     orderStatusUpdate = async (id) => {
-        let {status,notes} = this.state
-        await axios.put(`api/admin/orders/${id}`,{status,notes}).then(res => this.setState({orders: res.data}))
+        let { status, notes } = this.state
+        await axios.put(`api/admin/orders/${id}`, { status, notes }).then(res => this.setState({ orders: res.data }))
         this.getAllOrdersAdmin()
     }
     handleChange = (e) => {
-        this.setState({[e.target.name]:e.target.value})
+        this.setState({ [e.target.name]: e.target.value })
     }
     render() {
         console.log(this.state.orders)
-        let displayOrders = this.state.orders.map((elem,i)=>{
+        let displayOrders = this.state.orders.map((elem, i) => {
             return <div><Link key={i} to={`/orders/${elem.id}`}><div >Order:{elem.id}</div>
-                    {this.props.admin && <div><div>Customer: {elem.first_name} {elem.last_name}</div>
-                        <div>User ID: {elem.users_id}</div>
-                        <div>Contact {elem.phone}</div></div>}
-                    <div>Status:{elem.status}</div>
-                    <div>Date Created:{elem.created_at}</div>
-                    <div>Notes:{elem.notes}</div> </Link> {this.props.admin && <Popup className='modal' trigger={<button>Update</button>} position='right'>
-                <div>Update Status:</div>
-                <input defaultValue={elem.status} name='status' onChange={this.handleChange}/>
-                <div>Add Notes:</div>
-                <input defailtValue={elem.notes} name='notes' onChange={this.handleChange}/>
-                <button onClick={(e) => this.orderStatusUpdate(elem.id)}>Submit</button>
-                </Popup>}</div>
+                
+                {this.props.admin && <div><div>Customer: {elem.first_name} {elem.last_name}</div>
+                    <div>User ID: {elem.users_id}</div>
+                    <div>Contact {elem.phone}</div></div>}
+                <div>Status:{elem.status}</div>
+                <div>Date Created:{elem.created_at}</div>
+                <div>Notes:{elem.notes}</div> </Link> {this.props.admin && <Popup className='modal' trigger={<button>Update</button>} position='right'>
+                    <div>Update Status:</div>
+                    <input defaultValue={elem.status} name='status' onChange={this.handleChange} />
+                    <div>Add Notes:</div>
+                    <input defailtValue={elem.notes} name='notes' onChange={this.handleChange} />
+                    <button onClick={(e) => this.orderStatusUpdate(elem.id)}>Submit</button>
+                </Popup>}
+               
+                </div>
         })
-        return(
+        return (
             <div>
-            <h4>Order History</h4>
-            {displayOrders}</div>
+                <h4>Order History</h4>
+                {displayOrders}</div>
         )
     }
 }
-function mapStateToProps (state) {
-    return {admin: state.client.admin,
-            adminOrder_id: state.admin.adminOrder_id,
-            authenticated: state.client.authenticated
-}
+function mapStateToProps(state) {
+    return {
+        admin: state.client.admin,
+        adminOrder_id: state.admin.adminOrder_id,
+        authenticated: state.client.authenticated
+    }
 }
 export default connect(mapStateToProps)(Orders)
