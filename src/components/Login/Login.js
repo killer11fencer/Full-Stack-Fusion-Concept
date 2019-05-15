3ba4a682b22f557f1d7eef63e76945c25ced1bb9
Jeoffrey <jeoff.howe@gmail.com>
2019-05-14 23:01:51 -0600

    login css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,55 +1,63 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {withRouter} from 'react-router-dom'
-import {updateUsername, updateUserId,updateAdmin,updateAuthenticated} from '../../redux/reducer'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import { updateUsername, updateUserId, updateAdmin, updateAuthenticated } from '../../redux/reducer'
 
 class Login extends Component {
     constructor() {
         super();
-        this.state = { 
+        this.state = {
             loginUsername: '',
             loginPassword: '',
             loginError: false,
             loginErrorMessage: 'Username or Password is incorrect. Please try again',
             admin: false,
             authenticated: false,
-           
+
 
         }
     }
     handleChange = (e) => {
-        this.setState({[e.target.name]:e.target.value})
+        this.setState({ [e.target.name]: e.target.value })
     }
     handleSubmit = async (e) => {
-        const {loginUsername,loginPassword} = this.state
+        const { loginUsername, loginPassword } = this.state
         try {
-            const res = await axios.post('/auth/login',{loginUsername,loginPassword})
+            const res = await axios.post('/auth/login', { loginUsername, loginPassword })
             this.props.updateUsername(loginUsername)
             this.props.updateUserId(res.data.user_id)
             this.props.updateAdmin(res.data.admin)
             this.props.updateAuthenticated(res.data.authenticated)
             this.props.history.push('/menu')
-        } catch(err) {
-            this.setState({loginUsername: '',loginPassword:'',loginError:true,authenticated:false})
+        } catch (err) {
+            this.setState({ loginUsername: '', loginPassword: '', loginError: true, authenticated: false })
         }
     }
     render() {
-        
-        return(
+
+        return (
             <div className='loginScreen'>
-            <div className='Login'>
-            <div className='title'>LOGIN</div>
-            <div className='inputNames'>Username</div>
-            <input name='loginUsername' placeholder='Username' value={this.state.loginUsername} onChange={this.handleChange}/>
-            <div className='inputNames'>Password</div>
-            <input name='loginPassword' placeholder='Password' value={this.state.loginPassword} onChange={this.handleChange}/>
-           <button className='button'onClick={this.handleSubmit}>Login</button>
-           <Link to='/'><button className='button' >Cancel</button></Link>
-            <div>Not a member? <Link to='/register'>Register Here</Link></div>
-            {this.state.loginError && <h3>{this.state.loginErrorMessage}</h3>}
-            </div>
+                <img className='background' src='https://raster-static.postmates.com/?url=com.postmates.img.prod.s3.amazonaws.com/c04cf4c5-76d7-4007-a5c4-25dcf9328cdf/orig.jpg&quality=90&w=1500&h=900&mode=crop&format=jpg&v=4' />
+                <div className='Login'>
+                    <div className='title'>LOGIN</div>
+                    <div className='inputNames'>Username</div>
+                    <input name='loginUsername' placeholder='Username' value={this.state.loginUsername} onChange={this.handleChange} />
+                    <div className='inputNames'>Password</div>
+                    <input name='loginPassword' placeholder='Password' value={this.state.loginPassword} onChange={this.handleChange} />
+                    <button className='button' onClick={this.handleSubmit}>Login</button>
+                    <Link to='/'><button className='button' >Cancel</button></Link>
+                    <div>Not a member? <Link to='/register'>Register Here</Link></div>
+                    {this.state.loginError && <h3>{this.state.loginErrorMessage}</h3>}
+                </div>
+
+                <div className='address'>
+                    <div>Fusion Asian</div>
+                    <div>Address: 1469 Center st, <br /> Provo,UT 84660</div>
+                    <div>Phone: 805 611 91121</div>
+                </div>
+
             </div>
         )
     }
@@ -60,4 +68,4 @@ const mapDispatchToProps = {
     updateAdmin,
     updateAuthenticated
 }
-export default connect(null,mapDispatchToProps)(withRouter(Login))
+export default connect(null, mapDispatchToProps)(withRouter(Login))
